4753a120ffac77fce5de1f89a881dbd8718953c1
Praveen <impraveenbosco@gmail.com>
2023-08-24 11:12:01 +0530

    second

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,18 @@
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import { RecoilRoot } from "recoil";
+import ErrorBoundary from "@/components/ErrorBoundary";
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
     <SessionProvider session={session}>
-      <RecoilRoot>
-        <Component {...pageProps} />
-      </RecoilRoot>
+      <ErrorBoundary>
+        <RecoilRoot>
+          <Component {...pageProps} />
+        </RecoilRoot>
+      </ErrorBoundary>
     </SessionProvider>
   );
 }
