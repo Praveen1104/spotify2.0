503e0194e4d4ce83b6f90a5cc1333cefc2a09d47
Praveen <impraveenbosco@gmail.com>
2023-08-25 14:02:11 +0530

    four

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,10 +1,12 @@
-import Image from "next/image";
+
 /*import {
   HomeIcon,
   ChartBarIcon,
   ClockIcon,
   DotsHorizontalIcon,
 } from "@heroicons/react/24/outline";*/
+import Image from "next/image";
+import image1 from "../public/pictures/image0.png"
 import { AiFillHome } from "react-icons/ai";
 import { HiChartBar } from "react-icons/hi";
 import { FaClock } from "react-icons/fa";
@@ -16,7 +18,7 @@ function Sidebar() {
   return (
     <section className="fixed top-0 z-40 flex flex-col p-4 items-center bg-black w-[90px] h-screen space-y-8">
       <Image
-        src="https://rb.gy/xkacau "
+        src={image1}
         alt=""
         width={56}
         height={56}
